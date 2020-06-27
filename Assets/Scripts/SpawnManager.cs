2f66b6378370eb9c4d85cea917867dc78840eb1d
Ross Holder <ross@apprefactory.ca>
2020-06-27 00:39:52 -0400

    Inaugural checkin

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnPos 
+        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
