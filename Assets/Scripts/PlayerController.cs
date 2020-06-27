d7171e080ec4221bcf1a5bd3d8d60a2f9b8dc58a
Ross Holder <ross@apprefactory.ca>
2020-06-27 13:46:18 -0400

    Collsion with enemy marble setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f; 
+    public float speed = 5f;
+    public bool hasPowerup = false;
     private Rigidbody playerRb;
     private GameObject focalPoint;
 
@@ -21,5 +22,22 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup.ToString()); 
+        }
     }
 }
